28dd284006fb787958c6faf74488c70711176a09
shm878011 <shm878011@gmail.com>
2026-01-16 21:24:33 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.65 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int n, String control) {
-        String[] wasd = control.split("");
-        for(int i=0; i<wasd.length;i++){
-            if(wasd[i].equals("w"))
-                n+=1;
-            else if(wasd[i].equals("s"))
-                n-=1;
-            else if(wasd[i].equals("d"))
-                n+=10;
-            else if(wasd[i].equals("a"))
-                n-=10;
+        int answer = 0;
+        for (char ch: control.toCharArray()){
+        switch(ch){
+            case 'w': n++; break;
+            case 's': n--; break;
+            case 'd': n+=10; break;
+            case 'a': n-=10; break;
+            default: break;
         }
-        int answer = n;
+        }
+        answer = n;
         return answer;
     }
 }
