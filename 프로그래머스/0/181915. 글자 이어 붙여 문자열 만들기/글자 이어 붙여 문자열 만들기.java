6169ebba733aeccc3b7ead843aee263c078f3fd3
shm878011 <shm878011@gmail.com>
2026-01-17 17:45:30 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 13.76 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        char[] word = my_string.toCharArray();
+        for(int i=0; i<index_list.length; i++){
+            answer+=word[index_list[i]];
+        }
+        return answer;
+    }
+}
