7c0b067bb3d7dc3090dc09d66f1f2b6e66b66feb
shm878011 <shm878011@gmail.com>
2026-01-23 20:34:08 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14276 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,13 +7,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());
         char[] num = br.readLine().toCharArray();
-        int result = 0;
+        long result = 0;
+        long mod = 1234567891L;
+        long pow = 1;
         //System.out.println(num[1]-'a');
         for(int i=0; i<length; i++){
-            result+= (((num[i] - 'a')+1)* Math.pow(31,i));
+            //result+= (((num[i] - 'a')+1)* Math.pow(31,i));
+            long value = (num[i]-'a') +1 ;
+            result = (result+value *pow)%mod;
+            pow = pow *31 % mod;
         }
-        System.out.println(result%1234567891);
-       
-        
+        System.out.println(result);
+           
     }
 }
