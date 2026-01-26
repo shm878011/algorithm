493b508ea1aaaae6efa32e443918f1305c02b5ae
shm878011 <shm878011@gmail.com>
2026-01-26 20:21:32 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2380 ms, Memory: 360876 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num[] = new int[n];
+        for(int i=0; i<n; i++){
+            num[i]= Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(num);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            sb.append(num[i]).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
